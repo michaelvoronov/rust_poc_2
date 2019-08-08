33134ac864fe23ca93250a34a1cc5005c685d899
vms <michail.vms@gmail.com>
2019-08-08 18:18:09 +0300

    minimize poc

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,2 @@
-use fluence::sdk::*;
-use std::fs::File;
-use std::io::prelude::*;
-
-#[invocation_handler]
-fn main() -> String {
-    let mut file = File::create("foo.txt").unwrap();
-    file.write_all(b"Hello, world!").unwrap();
-
-    let mut file = File::open("foo.txt").unwrap();
-    let mut contents = String::new();
-    file.read_to_string(&mut contents).unwrap();
-    assert_eq!(contents, "Hello, world!");
-
-    "success!".to_string()
+fn main() {
 }
